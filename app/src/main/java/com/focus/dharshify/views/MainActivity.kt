56cdc4eb1357b439caaeaf7cb6a04dc2f086196c
priya <rajmuniya661@gmail.com>
2024-09-15 23:58:30 +0530

    Fixed the crash issue

--- app/src/main/java/com/focus/dharshify/views/MainActivity.kt
+++ app/src/main/java/com/focus/dharshify/views/MainActivity.kt
@@ -1,19 +1,45 @@
 package com.focus.dharshify.views
 
+import android.media.FaceDetector
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.focus.dharshify.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = findNavController(R.id.nav_host_fragment)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        loadFragment(HomePageFragment())
+        Log.i("isTest","check")
+        bottomNav = findViewById(R.id.bottom_navigation) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    loadFragment(HomePageFragment())
+                    true
+                }
+                R.id.musicFragment -> {
+                    loadFragment(MusicFragment())
+                    true
+                }
+                R.id.favoriteFragment -> {
+                    loadFragment(FavoriteFragment())
+                    true
+                }
+                else -> false
+            }
+        }
 
-        bottomNavigationView.setupWithNavController(navController)
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment,fragment)
+        transaction.commit()
     }
 }
