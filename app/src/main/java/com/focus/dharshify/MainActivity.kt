5a4e635ce410a8094813ba50617411c31c3b0c3b
priya <rajmuniya661@gmail.com>
2024-09-15 23:36:01 +0530

    Added he initial fragments

--- app/src/main/java/com/focus/dharshify/MainActivity.kt
+++ app/src/main/java/com/focus/dharshify/MainActivity.kt
@@ -3,11 +3,18 @@ package com.focus.dharshify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("isTestCommit","commit")
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Setup BottomNavigationView with NavController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
